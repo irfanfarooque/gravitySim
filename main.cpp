d0f026638c359191091b1c2bd771162d6bcd850f
achuzz-code <aswinuday2006@gmail.com>
2025-11-30 06:52:06 +0530

    the segfault has been fixed

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 
 #include <GLFW/glfw3.h>
+void print(const char *message) { std::cout << message << std::endl; }
 int main() {
   const char *vertexShaderSource = R"(
 #version 330 core
@@ -34,19 +35,38 @@ void main()
 }
   )";
   glfwInit();
+  std::cout << "GLFW initialized successfully" << std::endl;
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  GLFWwindow *window = glfwCreateWindow(800, 600, "OpenGL Window", NULL, NULL);
+  if (window == nullptr) {
+    std::cout << "Failed to create GLFW window" << std::endl;
+    glfwTerminate();
+    return -1;
+  }
+  glfwMakeContextCurrent(window);
+
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    std::cout << "Failed to initialize GLAD" << std::endl;
+    return -1;
+  }
+
+  glViewport(0, 0, 800, 600);
   GLfloat vertexData[] = {-1.0f, -1.0f, 0.0f, 1.0f, -1.0f,
                           0.0f,  0.0f,  1.0f, 0.0f};
+  std::cout << "Vertex data defined" << std::endl;
   GLuint vShader = glCreateShader(GL_VERTEX_SHADER);
+  print("vertex shader created");
   glShaderSource(vShader, 1, &vertexShaderSource, NULL);
+  print("shader source set");
   glCompileShader(vShader);
-
+  std::cout << "Vertex shader compiled successfully" << std::endl;
   GLuint fShader = glCreateShader(GL_FRAGMENT_SHADER);
   glShaderSource(fShader, 1, &fragmentShaderSource, NULL);
   glCompileShader(fShader);
-
+  std::cout << "fragment Shaders compiled successfully" << std::endl;
   GLuint shaderProgram = glCreateProgram();
   glAttachShader(shaderProgram, vShader);
   glAttachShader(shaderProgram, fShader);
@@ -56,14 +76,6 @@ void main()
   glDeleteShader(vShader);
   glDeleteShader(fShader);
 
-  GLFWwindow *window = glfwCreateWindow(800, 600, "OpenGL Window", NULL, NULL);
-  if (window == nullptr) {
-    std::cout << "Failed to create GLFW window" << std::endl;
-    glfwTerminate();
-    return -1;
-  }
-  glfwMakeContextCurrent(window);
-  glViewport(0, 0, 800, 600);
   while (!glfwWindowShouldClose(window)) {
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
